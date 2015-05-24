243bcc9e7afe88b345e527b79dd0a776d4b982e6
Michael P <info@cmsbased.net>
2015-05-24 03:34:06 -0400

    initial commit

---
+++ hebrew.php
@@ -0,0 +1,31 @@
+<?php 
+
+$_LANG["quickserverlogins"] = "Quick Server Logins";
+
+$_LANG['clientareadiskusage'] = "Disk Space";
+$_LANG['clientareabwusage'] = "Bandwidth";
+
+
+$_LANG['cpanellogin'] = "cPanel &#xf08e;";
+$_LANG['cpanelwhmlogin'] = "WHM &#xf08e;";
+$_LANG['cpanelwebmaillogin'] = "Webmail &#xf08e;";
+$_LANG['enkompasslogin'] = "Enkompass &#xf08e;";
+$_LANG['plesklogin'] = "Plesk &#xf08e;";
+$_LANG['helmlogin'] = "Helm &#xf08e;";
+$_LANG['siteworxlogin'] = "SiteWorx &#xf08e;";
+$_LANG['nodeworxlogin'] = "NodeWorx &#xf08e;";
+$_LANG['veportallogin'] = "vePortal &#xf08e;";
+$_LANG['virtualminlogin'] = "Control Panel &#xf08e;";
+$_LANG['websitepanellogin'] = "Control Panel &#xf08e;";
+$_LANG['whmsoniclogin'] = "Control Panel &#xf08e;";
+$_LANG['xpanelmaillogin'] = "Webmail &#xf08e;";
+$_LANG['xpanellogin'] = "XPanel &#xf08e;";
+$_LANG['heartinternetlogin'] = "Control Panel &#xf08e;";
+$_LANG['gamecplogin'] = "GameCP &#xf08e;";
+$_LANG['enomtrustelogin'] = "TrustE &#xf08e;";
+$_LANG['directadminlogin'] = "DirectAdmin &#xf08e;";
+$_LANG['centovacastlogin'] = "Centova Cast &#xf08e;";
+$_LANG['castcontrollogin'] = "Control Panel &#xf08e;";
+$_LANG['centovacastlogin'] = "Centova Cast &#xf08e;";
+
+$_LANG["quickserverdownload"] = "&#xf0ed;";
