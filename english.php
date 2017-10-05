50fd67f7df850234357077bbaed90c2f791218b7
Michael P <info@cmsbased.net>
2017-10-04 21:02:03 -0400

    duplicate removed.

--- english.php
+++ english.php
@@ -80,6 +80,5 @@ $_LANG['ticketratingquestion'] = "Rate this Reply:";
 $_LANG['ticketreatinggiven'] = "You Rated:";
 $_LANG['donthaveanaccount'] = "Don't have an account?";
 $_LANG['signinwith'] = "Sign in with";
-$_LANG['signinwith'] = "Sign in with";
 
 ?>
