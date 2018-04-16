10bc54934040313a5e903b504bcc73791b4b33b8
Argiris <BlackDragonGR@users.noreply.github.com>
2018-04-16 08:51:41 +0300

    Update greek.php

--- greek.php
+++ greek.php
@@ -2,7 +2,7 @@
 $_LANG['warning'] = "Warning!";
 $_LANG['navbarwelcome'] = "Welcome";
 $_LANG['accountcredit'] = "Account Credit";
-$_LANG["answersecurityquestion"] = "Please enter your answer";
+$_LANG['answersecurityquestion'] = "Please enter your answer";
 $_LANG['actions'] = "Actions";
 $_LANG['affiliatesreferals'] = "Your Referrals";
 $_LANG['captchaverify'] = "Please enter the characters you see in the image below into the text box provided.";
@@ -50,7 +50,7 @@ $_LANG['orderbillingcycle'] = "Billing";
 $_LANG['dashboard'] = "Dashboard";
 $_LANG['dashboardintro'] = "Welcome to your Dashboard area. Here you can manage your web hosting services, domains, view quotes, pay invoices and open support tickets."; 
 $_LANG['availcreditbaldesc'] = "You have a credit balance of %s";
-$_LANG["quickserverlogins"] = "Quick Server Logins";
+$_LANG['quickserverlogins'] = "Quick Server Logins";
 $_LANG['clientareadiskusage'] = "Disk Space";
 $_LANG['clientareabwusage'] = "Bandwidth";
 $_LANG['domainadd'] = "Add Domain";
@@ -63,7 +63,6 @@ $_LANG['flowcartyears'] = "Years";
 $_LANG['flowcartyear'] = "Year";
 $_LANG['flowcartmonth'] = "month";
 $_LANG['flowcartoptions'] = "Options";
-
 $_LANG['accountnotifications'] = "Account Notifications";
 $_LANG['openserviceissues'] = "Open Service Issues";
 $_LANG['scheduledservice'] = "Scheduled Service";
